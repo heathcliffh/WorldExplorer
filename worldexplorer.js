33ec41282f4c74fa9e53bbfa06e7283696fc72e9
Heathcliff Howard <132654319+heathcliffh@users.noreply.github.com>
2023-05-05 12:51:57 +0100

    Initial commit
    - Basic HTML and Javascript website

---
+++ worldexplorer.js
@@ -0,0 +1,46 @@
+  // Define an object containing the towns/cities of England and their corresponding counties
+  const towns = {
+    "London": "Greater London",
+    "Manchester": "Greater Manchester",
+    "Birmingham": "West Midlands",
+    "Leeds": "West Yorkshire",
+    "Liverpool": "Merseyside",
+    "Bristol": "Bristol",
+    "Sheffield": "South Yorkshire",
+    "Newcastle": "Tyne and Wear",
+    "Nottingham": "Nottinghamshire",
+    "Leicester": "Leicestershire",
+    "Brighton": "East Sussex",
+    "Oxford": "Oxfordshire",
+    "Cambridge": "Cambridgeshire",
+    "York": "North Yorkshire",
+    "Canterbury": "Kent",
+    "Bath": "Somerset"
+  };
+  
+  function calculatePercentage() {
+    // Get the user's input
+    const userInput = document.getElementById("towns").value;
+
+    // Split the input into an array of towns/cities
+    const userTowns = userInput.split(",");
+
+    // Define variables for counting the number of towns/cities visited and the total number of towns/cities in England
+    let visitedCount = 0;
+    const totalCount = Object.keys(towns).length;
+
+    // Loop through the user's towns/cities and count the ones that are in England
+    for (let i = 0; i < userTowns.length; i++) {
+      const town = userTowns[i].trim();
+      if (towns[town]) {
+        visitedCount++;
+      }
+    }
+
+    // Calculate the percentage of England visited
+    const percentage = Math.round((visitedCount / totalCount) * 100);
+
+    // Display the percentage on the page
+    const progressDiv = document.getElementById("progress");
+    progressDiv.innerHTML = `You have visited ${percentage}% of England.`;
+  }
